8a3f59cb016935a5052c2ae54c9c61c063e0338d
rohit__143 <rohitkumpan01@gmail.com>
2020-12-27 14:32:35 +0530

    serve start

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+const port=3000;
+
+
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+});
+
+
+
+
+
+app.listen(port,()=>{
+    console.log('running at port: ',port);
+})
